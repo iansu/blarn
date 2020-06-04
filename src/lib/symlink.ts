f45f6c2ab998ffef7f940e1c1b932556aa7c43ff
Ian Sutherland <ian@iansutherland.ca>
2020-06-03 18:17:57 -0600

    Fix bug with symlinks that point to non-existent target

--- src/lib/symlink.ts
+++ src/lib/symlink.ts
@@ -3,7 +3,8 @@ import path from 'path';
 
 interface Symlink {
   name: string;
-  target: string;
+  target?: string;
+  targetExists: boolean;
 }
 
 const getSymlinks = (directory: string): Symlink[] => {
@@ -12,10 +13,20 @@ const getSymlinks = (directory: string): Symlink[] => {
 
   for (const entry of entries) {
     if (entry.isSymbolicLink()) {
-      symlinks.push({
-        name: path.join(directory, entry.name),
-        target: fs.realpathSync(path.join(directory, entry.name))
-      });
+      try {
+        const target = fs.realpathSync(path.join(directory, entry.name));
+
+        symlinks.push({
+          name: path.join(directory, entry.name),
+          target,
+          targetExists: true
+        });
+      } catch (error) {
+        symlinks.push({
+          name: path.join(directory, entry.name),
+          targetExists: false
+        });
+      }
     } else if (entry.isDirectory()) {
       symlinks.push(...getSymlinks(path.join(directory, entry.name)));
     }
